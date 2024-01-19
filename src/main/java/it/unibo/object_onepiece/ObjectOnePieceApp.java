0e6038554e057d93a72bbead44f652fbfc32431b
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-01-19 18:59:38 +0100

    added basic healthbar view

--- src/main/java/it/unibo/object_onepiece/ObjectOnePieceApp.java
+++ src/main/java/it/unibo/object_onepiece/ObjectOnePieceApp.java
@@ -4,12 +4,18 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -24,10 +30,23 @@ public final class ObjectOnePieceApp extends Application {
     @Override
     public void start(final Stage primaryStage) throws Exception {
         primaryStage.setTitle("Object One Piece!");
-        
+
+        BorderPane borderPane = new BorderPane();
+
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
         gridPane.setGridLinesVisible(true);
+        
+        Canvas health = new Canvas(100, 100);
+        GraphicsContext healthGC = health.getGraphicsContext2D();
+        Rectangle healthBar = new Rectangle(20, 100);
+        drawHealthBar(healthGC, healthBar);
+
+        Label healthPoints = new Label("100");
+
+        VBox healthContainer = new VBox();
+        healthContainer.getChildren().addAll(health, healthPoints);
+
         Button buttons[] = new Button[MAP_ROWS * MAP_COLUMNS];
         for (int i = 0; i < MAP_ROWS; i++) {
             for (int j = 0; j < MAP_COLUMNS; j++) {
@@ -37,9 +56,21 @@ public final class ObjectOnePieceApp extends Application {
             }
         }
 
-        Scene scene = new Scene(gridPane, 600, 600);
+        borderPane.setCenter(gridPane);
+        borderPane.setRight(healthContainer);
+        Scene scene = new Scene(borderPane, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void drawHealthBar(GraphicsContext gc,Rectangle rect) {
+        gc.setFill(Color.GREEN);
+        gc.fillRect(rect.getX(),      
+               rect.getY(), 
+               rect.getWidth(), 
+               rect.getHeight());
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
     }
 
     /**
