8793a66a696fdc163c3e4a25659cdc705ab72b63
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-01-24 18:06:39 +0100

    added optional colors to enum

--- src/main/java/it/unibo/object_onepiece/view/EntityView.java
+++ src/main/java/it/unibo/object_onepiece/view/EntityView.java
@@ -1,9 +1,19 @@
 package it.unibo.object_onepiece.view;
 
+import java.util.Optional;
+
+import javafx.scene.paint.Color;
+
 public enum EntityView {
-    WATER,
-    PLAYER,
-    ENEMY,
-    BARREL,
-    ISLAND
+    WATER(Optional.of(Color.rgb(2, 127, 222))),
+    PLAYER(Optional.empty()),
+    ENEMY(Optional.empty()),
+    BARREL(Optional.empty()),
+    ISLAND(Optional.empty());
+
+    public final Optional<Color> color;
+
+    private EntityView(Optional<Color> c) {
+        this.color = c;
+    }
 }
