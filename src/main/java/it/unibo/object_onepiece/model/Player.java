1cd9ff87ce62bbb252ce63c045a0ac75c4386ecf
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-01-25 15:48:24 +0100

    Changed var names again

--- src/main/java/it/unibo/object_onepiece/model/Player.java
+++ src/main/java/it/unibo/object_onepiece/model/Player.java
@@ -6,7 +6,7 @@ public interface Player extends Ship {
     public void getExperience();
     public void addExperience(int experience);
 
-    public void heal(int health);
+    public void heal(int healthGained);
 
     public void interact();
 }
