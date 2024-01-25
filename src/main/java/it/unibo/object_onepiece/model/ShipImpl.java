d4c03d4927f02e16dcb7b7c82f218937703f500d
Matteo Giorgini <matteo.giorgini12@studio.unibo.it>
2024-01-25 15:35:00 +0100

    Changed Ship Design

--- src/main/java/it/unibo/object_onepiece/model/ShipImpl.java
+++ src/main/java/it/unibo/object_onepiece/model/ShipImpl.java
@@ -8,33 +8,32 @@ public abstract class ShipImpl extends EntityImpl implements Ship {
         throw new UnsupportedOperationException("Unimplemented method 'move'");
     }
 
-    private void rotate(Direction direction) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'rotate'");
-    }
-
     @Override
     public boolean shoot(Position position) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'shoot'");
     }
-
+    
     @Override
     public void takeDamage(int damage) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'takeDamage'");
     }
-
+    
     @Override
     public int getHealth() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getHealth'");
     }
-
+    
     @Override
     public Direction getDirection() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getDirection'");
     }
     
+    private void rotate(Direction direction) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'rotate'");
+    }
 }
