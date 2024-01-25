09e02091823c0f36b476b528df78ed2a2dc9c599
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-01-25 14:49:57 +0100

    small refactor

--- src/main/java/it/unibo/object_onepiece/view/EntityView.java
+++ src/main/java/it/unibo/object_onepiece/view/EntityView.java
@@ -2,12 +2,36 @@ package it.unibo.object_onepiece.view;
 
 import java.util.Optional;
 
+import javax.swing.text.html.Option;
+
+import it.unibo.object_onepiece.view.ObjectOnePieceApp.Entity;
 import javafx.scene.paint.Color;
 
 public class EntityView {
 
-    private Optional<Color> color;
-    private static enum Type {
+    private static final Color DEFAULT_COLOR = Color.rgb(2, 127, 222);
+
+    private Color color = DEFAULT_COLOR;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public EntityType getType() {
+        return type;
+    }
+
+    public void setType(EntityType type) {
+        this.type = type;
+    }
+
+    private EntityType type = EntityType.WATER;
+
+    private static enum EntityType {
         BARREL,
         ISLAND,
         ENEMY,
@@ -15,8 +39,11 @@ public class EntityView {
         WATER
     }
 
+    public EntityView() {
 
-    public EntityView(final Optional<Color> c) {
+    }
+
+    public EntityView(final Color c) {
         this.color = c;
     }
 }
