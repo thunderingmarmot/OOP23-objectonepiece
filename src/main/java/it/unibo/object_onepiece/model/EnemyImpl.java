4bd6b69f98cc6bd918a285ebffcd120022859950
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-01-25 17:26:41 +0100

    Fixed Enemy constructor

--- src/main/java/it/unibo/object_onepiece/model/EnemyImpl.java
+++ src/main/java/it/unibo/object_onepiece/model/EnemyImpl.java
@@ -1,6 +1,13 @@
 package it.unibo.object_onepiece.model;
 
+import it.unibo.object_onepiece.model.Utils.Direction;
+import it.unibo.object_onepiece.model.Utils.Position;
+
 public class EnemyImpl extends ShipImpl implements Enemy {
+
+    public EnemyImpl(Position position, Direction direction, int health) {
+        super(position, direction, health);
+    }
 
     @Override
     public void goNext() {
