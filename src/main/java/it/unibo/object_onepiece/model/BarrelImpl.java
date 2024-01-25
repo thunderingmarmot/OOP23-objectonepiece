ec04f9d1b837e1edb49a687ecfd4652d4aa12421
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-01-25 16:32:36 +0100

    Fixed Barrel constructor

--- src/main/java/it/unibo/object_onepiece/model/BarrelImpl.java
+++ src/main/java/it/unibo/object_onepiece/model/BarrelImpl.java
@@ -1,10 +1,13 @@
 package it.unibo.object_onepiece.model;
 
+import it.unibo.object_onepiece.model.Utils.Position;
+
 public class BarrelImpl extends EntityImpl implements Barrel {
 
     private final int experienceGiven;
 
-    public BarrelImpl(int experienceGiven) {
+    public BarrelImpl(Position position, int experienceGiven) {
+        super(position);
         this.experienceGiven = experienceGiven;
     }
 
