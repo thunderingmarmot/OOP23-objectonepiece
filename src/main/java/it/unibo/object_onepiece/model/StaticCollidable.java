72fc81b122e9c79a2772110b93b0d71ed32cecba
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-01-28 12:07:27 +0100

    Introducing crashing mechanics again but with the new collision system

--- src/main/java/it/unibo/object_onepiece/model/StaticCollidable.java
+++ src/main/java/it/unibo/object_onepiece/model/StaticCollidable.java
@@ -1,7 +1,5 @@
 package it.unibo.object_onepiece.model;
 
-// Define what a Ship can collide with
-
 public interface StaticCollidable extends Collidable {
     public void onCollision(MovingCollidable collider);
 }
