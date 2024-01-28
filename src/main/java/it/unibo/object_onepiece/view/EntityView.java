c647cbc64e18f6c5b6b42199957a536790a08861
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-01-28 17:31:36 +0100

    small mod

--- src/main/java/it/unibo/object_onepiece/view/EntityView.java
+++ src/main/java/it/unibo/object_onepiece/view/EntityView.java
@@ -13,6 +13,8 @@ public class EntityView {
 
     private Color color = DEFAULT_COLOR;
 
+    public int direction = 0;
+
     public Color getColor() {
         return color;
     }
@@ -31,19 +33,55 @@ public class EntityView {
 
     private EntityType type = EntityType.WATER;
 
-    private static enum EntityType {
+    public static enum EntityType {
         BARREL,
         ISLAND,
         ENEMY,
         PLAYER,
-        WATER
+        WATER;
     }
 
     public EntityView() {
+    }
 
+    public EntityView(final EntityType type) {
+        this.type = type;
+        this.color = DEFAULT_COLOR;
+        this.direction = 0;
+    }
+
+    public EntityView(final EntityType type, final int direction) {
+        this.type = type;
+        this.color = DEFAULT_COLOR;
+        this.direction = direction;
     }
 
     public EntityView(final Color c) {
         this.color = c;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final EntityView other = (EntityView) obj;
+        if ((this.type == null) ? (other.type != null) : !this.type.equals(other.type)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + (this.type != null ? this.type.hashCode() : 0);
+        return hash;
     }
 }
