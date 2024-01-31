9577a06db0d2c260116dc9d26c5047a6736d2571
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-01-31 22:11:21 +0100

    Reverted EventImpl

---
+++ src/main/java/it/unibo/object_onepiece/model/events/EventImpl.java
@@ -0,0 +1,26 @@
+package it.unibo.object_onepiece.model.events;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class EventImpl<T extends EventArgs> implements Event<T> {
+
+    private List<Consumer<T>> listeners = new ArrayList<>();
+
+    @Override
+    public void subscribe(Consumer<T> listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void unsubscribe(Consumer<T> listener) {
+        listeners.remove(listener);
+    }
+
+    @Override
+    public void invoke(T args) {
+        listeners.forEach((listener) -> listener.accept(args));
+    }
+    
+}
--- src/main/java/it/unibo/object_onepiece/model/events/Events.java
+++
@@ -1,5 +0,0 @@
-package it.unibo.object_onepiece.model.events;
-
-public final class Events {
-
-}
