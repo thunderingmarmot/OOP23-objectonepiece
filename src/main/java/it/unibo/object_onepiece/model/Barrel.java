301c150f77e166dc953c0bba52d8e766578b111d
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-01-31 11:16:38 +0100

    Introduced Viewable interface to better manage entities in the View side

--- src/main/java/it/unibo/object_onepiece/model/Barrel.java
+++ src/main/java/it/unibo/object_onepiece/model/Barrel.java
@@ -3,6 +3,6 @@ package it.unibo.object_onepiece.model;
 /**
  * The barrels are collectable entities for the user
  */
-public interface Barrel extends Collidable {
+public interface Barrel extends Viewable, Collidable {
     public void take(Player player);
 }
--- src/main/java/it/unibo/object_onepiece/model/Player.java
+++ src/main/java/it/unibo/object_onepiece/model/Player.java
@@ -4,7 +4,7 @@ import it.unibo.object_onepiece.model.Ship.Ship;
 
 //Interface that models the player's ship
 
-public interface Player extends Ship {
+public interface Player extends Viewable, Ship {
     public int getExperience();
     public void addExperience(int experience);
 }
