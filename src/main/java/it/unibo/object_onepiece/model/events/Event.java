998998981a432baa3e71698a2de7625dca1070a5
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-01-31 22:05:41 +0100

    Reverted Event to original state

--- src/main/java/it/unibo/object_onepiece/model/events/Event.java
+++ src/main/java/it/unibo/object_onepiece/model/events/Event.java
@@ -1,8 +1,9 @@
 package it.unibo.object_onepiece.model.events;
 
-public interface Event {
-    public void subscribe();
-    public void unsubscribe();
+import java.util.function.Consumer;
 
+public interface Event {
+    public void subscribe(Consumer<EventArgs> listener);
+    public void unsubscribe(Consumer<EventArgs> listener);
     public void invoke();
 }
