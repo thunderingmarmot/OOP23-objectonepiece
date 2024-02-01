6feefedefcf9deaa402bb5cde047a12705cf6b12
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-02-01 09:54:57 +0100

    Implemented ObjectCreated event to put in Section onEntityCreation

--- src/main/java/it/unibo/object_onepiece/model/events/EventArgs.java
+++ src/main/java/it/unibo/object_onepiece/model/events/EventArgs.java
@@ -1,13 +1,18 @@
 package it.unibo.object_onepiece.model.events;
 
 public interface EventArgs {
+    public record Generic() implements EventArgs {}
+    public static Generic generic() {
+        return new Generic();
+    }
+
     public record ValueChanged<T>(T oldValue, T newValue) implements EventArgs {}
     public static <T> ValueChanged<T> valueChanged(T oldValue, T newValue) {
         return new ValueChanged<T>(oldValue, newValue);
     }
-    
-    public record Generic() implements EventArgs {}
-    public static Generic generic() {
-        return new Generic();
+
+    public record ObjectCreated<T>(T object) implements EventArgs {}
+    public static <T> ObjectCreated<T> objectCreated(T object) {
+        return new ObjectCreated<T>(object);
     }
 }
