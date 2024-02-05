15d1e45d1feb576b4ec238d024218497f77a5dff
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-02-05 17:16:01 +0100

    added getter for event

--- src/main/java/it/unibo/object_onepiece/model/Section.java
+++ src/main/java/it/unibo/object_onepiece/model/Section.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import it.unibo.object_onepiece.model.Utils.Bound;
 import it.unibo.object_onepiece.model.Utils.Position;
+import it.unibo.object_onepiece.model.events.Event;
+import it.unibo.object_onepiece.model.events.EventArgs.BiArguments;
 
 /**
 * Interface that models a Section of the world, it's itself divided in cells
@@ -18,6 +20,8 @@ public interface Section {
     public Optional<Entity> getEntityAt(Position position);
     public List<Entity> getEntities();
     public List<Viewable> getViewables();
+
+    public Event<BiArguments<Class<? extends Entity>, Position>> getEntityCreatedEvent();
 
     public void addEntity(Entity e);
     public void removeEntityAt(Position position);
