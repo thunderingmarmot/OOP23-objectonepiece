f11bcd3085675ba37ac7dbae5adc88f7b1f78e6b
Matteo Giorgini <matteo.giorgini12@studio.unibo.it>
2024-02-06 14:06:45 +0100

    Fixed Weapon indentation and weapons builder

--- src/main/java/it/unibo/object_onepiece/model/ship/Weapon.java
+++ src/main/java/it/unibo/object_onepiece/model/ship/Weapon.java
@@ -3,29 +3,44 @@ package it.unibo.object_onepiece.model.ship;
 import it.unibo.object_onepiece.model.Utils.Position;
 
 public interface Weapon extends ShipComponent {
-    public enum ShootDetails {
+    enum ShootDetails {
         SHOOTED_SUCCESSFULLY,
         OUT_OF_SHOOTING_RANGE,
         WEAPON_BROKEN
     };
 
-    public record ShootReturnType(boolean hasShooted, ShootDetails details){}; 
+    record ShootReturnType(boolean hasShooted, ShootDetails details) { }; 
 
-    public ShootReturnType shoot(Position position);
+    ShootReturnType shoot(Position position);
 
-    public int getMaxDamage();
-    public int getMinDamage();
-    public int getRange();
+    int getMaxDamage();
+    int getMinDamage();
+    int getRange();
 
-    public static Weapon cannon() {
-        return new WeaponImpl(20, 10, 3, 100);
+    static Weapon cannon() {
+        final int MAX_DAMAGE = 20;
+        final int MIN_DAMAGE = 10;
+        final int RANGE = 3;
+        final int HEALTH = 100;
+
+        return new WeaponImpl(MAX_DAMAGE, MIN_DAMAGE, RANGE, HEALTH);
     }
 
-    public static Weapon railgun() {
-        return new WeaponImpl(50, 0, 5, 70);
+    static Weapon railgun() {
+        final int MAX_DAMAGE = 50;
+        final int MIN_DAMAGE = 0;
+        final int RANGE = 5;
+        final int HEALTH = 70;
+
+        return new WeaponImpl(MAX_DAMAGE, MIN_DAMAGE, RANGE, HEALTH);
     }
 
-    public static Weapon heavycannon() {
-        return new WeaponImpl(80, 40, 1, 120);
+    static Weapon heavycannon() {
+        final int MAX_DAMAGE = 80;
+        final int MIN_DAMAGE = 40;
+        final int RANGE = 1;
+        final int HEALTH = 120;
+
+        return new WeaponImpl(MAX_DAMAGE, MIN_DAMAGE, RANGE, HEALTH);
     }
 }
