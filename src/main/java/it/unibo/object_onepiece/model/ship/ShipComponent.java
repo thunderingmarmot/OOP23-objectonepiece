3b9612e45c68259561635844472eca29b8440de9
Matteo Giorgini <matteo.giorgini12@studio.unibo.it>
2024-02-06 15:00:09 +0100

    removed redondant public in ShipComponent

--- src/main/java/it/unibo/object_onepiece/model/ship/ShipComponent.java
+++ src/main/java/it/unibo/object_onepiece/model/ship/ShipComponent.java
@@ -1,10 +1,10 @@
 package it.unibo.object_onepiece.model.ship;
 
 public interface ShipComponent {
-    public void setHealth(int health);
-    public void setShip(Ship ship);
+    void setHealth(int health);
+    void setShip(Ship ship);
 
-    public Ship getShip();
-    public int getHealth();
-    public int getMaxHealth();
+    Ship getShip();
+    int getHealth();
+    int getMaxHealth();
 }
