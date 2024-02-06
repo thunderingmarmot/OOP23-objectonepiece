28c2c53400318bf4140852ba40db00806adcfd2d
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-02-06 11:38:20 +0100

    Event is now checkstyle-violations free

--- src/main/java/it/unibo/object_onepiece/model/events/Event.java
+++ src/main/java/it/unibo/object_onepiece/model/events/Event.java
@@ -2,12 +2,39 @@ package it.unibo.object_onepiece.model.events;
 
 import java.util.function.Consumer;
 
+/**
+ * Models an Event which can be invoked by the observed and subscribed to by the observer.
+ * @param <T> the generic type implementing EventArgs defining the Event arguments
+ * @see EventArgs
+ */
 public interface Event<T extends EventArgs> {
-    public void subscribe(Consumer<T> listener);
-    public void unsubscribe(Consumer<T> listener);
-    public void invoke(T args);
+    /**
+     * Subscribe a Consumer<T> to the Event.
+     * @param listener
+     * @see Consumer
+     */
+    void subscribe(Consumer<T> listener);
 
-    public static <T extends EventArgs> Event<T> get() {
+    /**
+     * Unsubscribe a Consumer<T> to the Event.
+     * @param listener
+     * @see Consumer
+     */
+    void unsubscribe(Consumer<T> listener);
+
+    /**
+     * Invoke the Event with some arguments.
+     * @param args
+     * @see EventArgs
+     */
+    void invoke(T args);
+
+    /**
+     * Creates a new Event.
+     * @param <T> the genric type implementing EventArgs defining the Event arguments
+     * @return a newly created Event object with the matching generic EventArgs
+     */
+    static <T extends EventArgs> Event<T> get() {
         return new EventImpl<T>();
     }
 }
