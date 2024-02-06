40a9868f06df965955d8a153ab1cd4f0c44eaf5c
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-02-06 14:00:26 +0100

    BarrelImpl is now checkstyle-violations free.

--- src/main/java/it/unibo/object_onepiece/model/BarrelImpl.java
+++ src/main/java/it/unibo/object_onepiece/model/BarrelImpl.java
@@ -2,23 +2,45 @@ package it.unibo.object_onepiece.model;
 
 import it.unibo.object_onepiece.model.Utils.Position;
 
+/**
+ * Implementation of the Barrel interface.
+ * @see Barrel
+ */
 public class BarrelImpl extends EntityImpl implements Barrel {
 
     private final int experienceGiven;
 
-    protected BarrelImpl(Section section, Position position, int experienceGiven) {
+    /**
+     * Constructor for BarrelImpl.
+     * It's protected to only allow creating this object inside this package.
+     * Actual object creation is handled in the static method inside Barrel interface.
+     * @param section the reference to the Section containing this Barrel 
+     * @param position the position to place this Island at
+     * @param experienceGiven the experience value this Barrel gives the Player when taken
+     */
+    protected BarrelImpl(final Section section, final Position position, final int experienceGiven) {
         super(section, position);
         this.experienceGiven = experienceGiven;
     }
 
+    /**
+     * Defines the behaviour of getting taken by a Player.
+     * @param player the Player that is taking this Barrel
+     * @see Player
+     */
     @Override
-    public void take(Player player) {
+    public void take(final Player player) {
         player.addExperience(experienceGiven);
     }
 
+    /**
+     * A collision with a Barrel means destroying it and picking it up.
+     * @param collider the Collider that collided with the Barrel
+     * @see Collidable
+     */
     @Override
-    public void onCollisionWith(Collider collider) {
-        if(collider instanceof Player player) {
+    public void onCollisionWith(final Collider collider) {
+        if (collider instanceof Player player) {
             take(player);
         }
         this.remove();
