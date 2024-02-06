5a103b712c78f369714367079dabfe5d9f9f17a9
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-02-06 13:27:09 +0100

    Player is now checkstyle-violations free

--- src/main/java/it/unibo/object_onepiece/model/Player.java
+++ src/main/java/it/unibo/object_onepiece/model/Player.java
@@ -1,6 +1,7 @@
 package it.unibo.object_onepiece.model;
 
-import it.unibo.object_onepiece.model.Utils.*;
+import it.unibo.object_onepiece.model.Utils.Direction;
+import it.unibo.object_onepiece.model.Utils.Position;
 import it.unibo.object_onepiece.model.events.Event;
 import it.unibo.object_onepiece.model.events.EventArgs.Argument;
 import it.unibo.object_onepiece.model.ship.Bow;
@@ -8,19 +9,47 @@ import it.unibo.object_onepiece.model.ship.Sail;
 import it.unibo.object_onepiece.model.ship.Ship;
 import it.unibo.object_onepiece.model.ship.Weapon;
 
-//Interface that models the player's ship
-
+/**
+ * Models a particular Ship which is the Player of the game.
+ * This distinction is necessary as the Player will also have the possibility of gaining experience.
+ * @see Ship
+ * @see Viewable
+ */
 public interface Player extends Viewable, Ship {
-    public static Player getDefault(Section spawnSection, Position spawnPosition) {
+    /**
+     * Creates a default Player.
+     * @param spawnSection the reference to the Section containing this Player
+     * @param spawnPosition the position to place this Player at
+     * @return the newly created Player object
+     */
+    static Player getDefault(Section spawnSection, Position spawnPosition) {
         return new PlayerImpl(spawnSection, spawnPosition, Direction.UP, Weapon.cannon(), Sail.sloop(), Bow.standard(), 0);
     }
 
-    public int getExperience();
-    public void addExperience(int experience);
-    public Event<Argument<Integer>> getExperienceAddedEvent();
+    /**
+     * Getter for the experience private field.
+     * @return the currently owned experience value
+     */
+    int getExperience();
 
+    /**
+     * Adds experience to the Player's owned experience.
+     * @param experience the experience value to add
+     */
+    void addExperience(int experience);
+
+    /**
+     * Getter for the onExperienceAdded Event.
+     * @return an Event object that gets invoked when experience is added to the Player.
+     * @see Event
+     */
+    Event<Argument<Integer>> getExperienceAddedEvent();
+
+    /**
+     * @see Viewable
+     */
     @Override
-    public default Type getViewType() {
+    default Type getViewType() {
         return Type.PLAYER;
     }
 }
