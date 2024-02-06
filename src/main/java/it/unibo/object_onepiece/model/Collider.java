65e9aa52ca36fc2615308441d5deacfccc1e1c1d
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-02-06 13:46:41 +0100

    Collider is now checkstyle-violations free

--- src/main/java/it/unibo/object_onepiece/model/Collider.java
+++ src/main/java/it/unibo/object_onepiece/model/Collider.java
@@ -3,6 +3,15 @@ package it.unibo.object_onepiece.model;
 // A Collider is something that can collide with other Collidables,
 // that means that it must extend Movable, as only Movable things can collide
 
+/**
+ * Models a Collidable that is also a Movable.
+ * @see Collidable
+ * @see Movable
+ */
 public interface Collider extends Collidable, Movable {
-    public void collideWith(Collidable collidable);
+    /**
+     * Defines the behaviour when this Collider collides with a Collidable.
+     * @param collidable the Collidable this Collider collided with
+     */
+    void collideWith(Collidable collidable);
 }
