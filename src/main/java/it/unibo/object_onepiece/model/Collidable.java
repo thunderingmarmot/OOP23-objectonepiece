6008108d084b6e24469218e280b5d7aa95ac65a0
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-02-06 13:44:09 +0100

    Collidable is now checkstyle-violations free

--- src/main/java/it/unibo/object_onepiece/model/Collidable.java
+++ src/main/java/it/unibo/object_onepiece/model/Collidable.java
@@ -2,13 +2,39 @@ package it.unibo.object_onepiece.model;
 
 // A Collidable is something that can be collided with
 
+/**
+ * Models an Entity which a Collider can collide with.
+ * @see Entity
+ * @see Collider
+ */
 public interface Collidable extends Entity {
-    public enum Rigidness {
-        SOFT, // Gets instantly destroyed by ramming and can be passed on
-        MEDIUM, // Can be passed on after being destroyed by ramming
-        HARD, // Cannot be passed on or destroyed by ramming
+    /**
+     * Defines the possbile values of Rigidness.
+     */
+    enum Rigidness {
+        /**
+         * Collidable gets instantly destroyed by ramming and can be passed on.
+         */
+        SOFT, 
+        /**
+         * Collidable can be passed on only after being destroyed by ramming.
+         */
+        MEDIUM, 
+        /**
+         * Collidable cannot be passed on and cannot be destroyed by ramming.
+         */
+        HARD, 
     }
 
-    public Rigidness getRigidness();
-    public void onCollisionWith(Collider collider);
+    /**
+     * Getter for the Rigidness value of this Collidable.
+     * @return the Rigidness value
+     */
+    Rigidness getRigidness();
+
+    /**
+     * Defines the behaviour when a Collider collides with this Collidable.
+     * @param collider the Collider which collided
+     */
+    void onCollisionWith(Collider collider);
 }
