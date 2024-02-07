41de08e9ac7b6d88e15da7811cf6afdd88a97f04
Matteo Giorgini <matteo.giorgini12@studio.unibo.it>
2024-02-07 14:36:23 +0100

    Added comments to ShipComponent interface

--- src/main/java/it/unibo/object_onepiece/model/ship/ShipComponent.java
+++ src/main/java/it/unibo/object_onepiece/model/ship/ShipComponent.java
@@ -1,5 +1,10 @@
 package it.unibo.object_onepiece.model.ship;
 
+/**
+ * This interface is implemented by all the ship components.
+ * It defines the ship components common methods, like setting
+ * health and Ship, and getting health and maximum health.
+ */
 public interface ShipComponent {
     /**
      * Set method for the health of the ShipComponent.
