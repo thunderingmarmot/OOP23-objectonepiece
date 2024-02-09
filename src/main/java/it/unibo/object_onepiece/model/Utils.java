edd0bf26e298a518b7df6386c5e465adbc479fd4
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-02-09 18:12:20 +0100

    Fixed versorToCardinalDirections map

--- src/main/java/it/unibo/object_onepiece/model/Utils.java
+++ src/main/java/it/unibo/object_onepiece/model/Utils.java
@@ -32,9 +32,9 @@ public final class Utils {
 
     private static Map<Position, CardinalDirection> versorToCardinalDirections = Map.of(
         new Position(0, 1), CardinalDirection.EAST,
-        new Position(-1, 0), CardinalDirection.SOUTH,
+        new Position(-1, 0), CardinalDirection.NORTH,
         new Position(0, -1), CardinalDirection.WEST,
-        new Position(1, 0), CardinalDirection.NORTH
+        new Position(1, 0), CardinalDirection.SOUTH
     );
 
     private static List<BiPredicate<Bound, Position>> insideBoundsConditions = List.of(
