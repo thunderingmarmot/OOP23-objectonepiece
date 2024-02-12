ff1610418d9f15736f4d5c50cdb8adf509a582a0
Edoardo Scorza <edoardo.scorza@studio.unibo.it>
2024-02-12 13:17:59 +0100

    Attack state shooting implemented

--- src/main/java/it/unibo/object_onepiece/model/AttackState.java
+++ src/main/java/it/unibo/object_onepiece/model/AttackState.java
@@ -1,25 +1,39 @@
 package it.unibo.object_onepiece.model;
 
 import it.unibo.object_onepiece.model.Enemy.States;
-import it.unibo.object_onepiece.model.Ship.MoveDetails;
 import it.unibo.object_onepiece.model.Utils.CardinalDirection;
 import it.unibo.object_onepiece.model.Utils.Position;
 
+import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 
 import it.unibo.object_onepiece.model.Enemy.EnemyState;
 
 public class AttackState extends EnemyState {
     private Enemy ship;
+    private Player player;
+    private Integer triggerDistance = 3;
+    private Integer distanceFromPlayer = 3;
    
     public AttackState(Enemy ship) {
         this.ship = ship;
+        this.player = ship.getSection().getPlayer();
     }
 
     @Override
     public Boolean perform() {
-       
+        distanceFromPlayer = player.getPosition().distanceFrom(ship.getPosition());
+        if(distanceFromPlayer > triggerDistance){
+            ship.changeState(States.PATROLLING);
+            return false;
+        }
+       if(isInRange(ship.getDirection())){
+            if(ship.shoot(player.getPosition()).hasShooted()){
+                return true;
+            }
+
+       }
+       return true;
     }
 
 
@@ -28,9 +42,26 @@ public class AttackState extends EnemyState {
         return States.ATTACKING;
     }
 
-    private Position nextPos(final CardinalDirection direction) {
-        return Utils.getCardinalDirectionsTranslationMap()
-                .get(direction).apply(ship.getPosition());
+    private Boolean isInRange(CardinalDirection shipDirection){
+        if(distanceFromPlayer < ship.getWeapon().getRange()){
+            for (var direction : sidesOf(shipDirection)) {
+                if(ship.getPosition().isInlineWith(player.getPosition(), direction)){
+                    return true;
+                }
+            }
+        }
+        return false; 
+    }
+
+    private List<CardinalDirection> sidesOf(CardinalDirection direction){
+        var side1 = List.of(CardinalDirection.EAST,CardinalDirection.WEST);
+        var side2 = List.of(CardinalDirection.NORTH,CardinalDirection.SOUTH);
+        return Map.of(
+            CardinalDirection.NORTH,side1,
+            CardinalDirection.SOUTH,side1,
+            CardinalDirection.EAST,side2,
+            CardinalDirection.WEST,side2
+        ).get(direction);
     }
  
 }
