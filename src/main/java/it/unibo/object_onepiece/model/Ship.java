6d8d138733bc22b910fd56c708639d6834d69174
Matteo Giorgini <matteo.giorgini12@studio.unibo.it>
2024-02-12 13:06:03 +0100

    Removed useless import from Ship

--- src/main/java/it/unibo/object_onepiece/model/Ship.java
+++ src/main/java/it/unibo/object_onepiece/model/Ship.java
@@ -3,7 +3,6 @@ package it.unibo.object_onepiece.model;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Random;
 
 import it.unibo.object_onepiece.model.Weapon.ShootReturnType;
 import it.unibo.object_onepiece.model.Weapon.ShootDetails;
