cfc03e945b597790185903d206a2b128f1ff3626
Tommaso De Tommaso <tommaso.detommaso@studio.unibo.it>
2024-02-12 09:53:18 +0100

    Re-introducing old Event system, but better

---
+++ src/main/java/it/unibo/object_onepiece/model/events/EventArgs.java
@@ -0,0 +1,59 @@
+package it.unibo.object_onepiece.model.events;
+
+/**
+ * Models an object that can be used as a container for Event arguments.
+ */
+public interface EventArgs {
+    /**
+     * Defines an Event arguments container of no arguments.
+     */
+    record NoArgument() implements EventArgs { }
+
+    /**
+     * Defines an Event arguments container of one generic argument.
+     * @param <T> the generic type for the only argument
+     * @param arg the only argument of type T
+     */
+    record Argument<T>(T arg) implements EventArgs { }
+
+    /**
+     * Defines an Event arguments container of two arguments of the same generic type.
+     * @param <T> the generic type for both arguments
+     * @param arg1 the first argument of type T
+     * @param arg2 the second argument of type T
+     */
+    record BiArgument<T>(T arg1, T arg2) implements EventArgs { }
+
+    /**
+     * Defines an Event arguments container of two generic arguments.
+     * @param <T1> the generic type of the first argument
+     * @param <T2> the generic type of the second argument
+     * @param arg1 the first argument of type T1
+     * @param arg2 the second argument of type T2
+     */
+    record BiArguments<T1, T2>(T1 arg1, T2 arg2) implements EventArgs { }
+
+    /**
+     * Defines an Event arguments container of three generic arguments.
+     * @param <T1> the generic type of the first argument
+     * @param <T2> the generic type of the second argument
+     * @param <T3> the generic type of the third argument
+     * @param arg1 the first argument of type T1
+     * @param arg2 the second argument of type T2
+     * @param arg3 the third argument of type T3
+     */
+    record TriArguments<T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3) implements EventArgs { }
+
+    /**
+     * Defines an Event arguments container of four generic arguments.
+     * @param <T1> the generic type of the first argument
+     * @param <T2> the generic type of the second argument
+     * @param <T3> the generic type of the third argument
+     * @param <T4> the generic type of the fourth argument
+     * @param arg1 the first argument of type T1
+     * @param arg2 the second argument of type T2
+     * @param arg3 the third argument of type T3
+     * @param arg4 the fourth argument of type T4
+     */
+    record QuadArguments<T1, T2, T3, T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4) implements EventArgs { }
+}
