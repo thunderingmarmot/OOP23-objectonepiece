3beeb68a4fd1338d4ee856fae54a6e64af140da1
Matteo Giorgini <matteo.giorgini12@studio.unibo.it>
2024-02-14 16:48:50 +0100

    Added pause ambience sound button

--- src/main/java/it/unibo/object_onepiece/view/Sound.java
+++ src/main/java/it/unibo/object_onepiece/view/Sound.java
@@ -17,7 +17,6 @@ import java.net.URL;
  */
 public final class Sound {
     enum SoundTypes {
-        AMBIENCE,
         CANNON_SHOT,
         SHIP_COLLIDE,
         SHIP_DESTROY,
@@ -29,7 +28,6 @@ public final class Sound {
     private static final String SOUND_FOLDER = "sound/";
 
     private final Map<SoundTypes, URL> soundTypesToFile = Map.of(
-        SoundTypes.AMBIENCE, this.getURL(SOUND_FOLDER + "franky_theme.wav"),
         SoundTypes.CANNON_SHOT, this.getURL(SOUND_FOLDER + "cannon_shot.wav"),
         SoundTypes.SHIP_COLLIDE, this.getURL(SOUND_FOLDER + "ship_collide.wav"),
         SoundTypes.SHIP_DESTROY, this.getURL(SOUND_FOLDER + "ship_destroy.wav"),
@@ -37,6 +35,10 @@ public final class Sound {
         SoundTypes.BARRELL_DESTROY, this.getURL(SOUND_FOLDER + "cannon_shot.wav"),
         SoundTypes.MINE_DESTROY, this.getURL(SOUND_FOLDER + "mine_explode.wav")
     );
+
+    private final URL ambienceSoundURL = this.getURL(SOUND_FOLDER + "franky_theme.wav");
+    private AudioInputStream ambienceAudioIN;
+    private Clip ambienceClip;
 
     /**
      * This method play a specific sound based on the received SoundTypes.
@@ -48,7 +50,9 @@ public final class Sound {
      */
     void playSound(final SoundTypes sound) 
     throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        this.play(soundTypesToFile.get(sound), 0, false);
+        final AudioInputStream audioIN = AudioSystem.getAudioInputStream(soundTypesToFile.get(sound));
+        final Clip clip = AudioSystem.getClip();
+        this.play(audioIN, clip, false);
     }
 
     /**
@@ -59,22 +63,38 @@ public final class Sound {
      * @throws         IOException
      * @throws         LineUnavailableException
      */
-    void playAmbienceSound(final float volume)
+    void playAmbienceSound()
     throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        this.play(soundTypesToFile.get(SoundTypes.AMBIENCE), volume, true);
+        this.ambienceAudioIN = AudioSystem.getAudioInputStream(ambienceSoundURL);
+        this.ambienceClip = AudioSystem.getClip();
+        this.play(this.ambienceAudioIN, this.ambienceClip, false);
+    }
+
+    void setVolume(final Clip clip, final float volume) {
+        final FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        volumeControl.setValue(volume);
+    }
+
+    void pauseSound(final Clip clip) {
+        if(clip.isRunning()) {
+            clip.stop();
+        } else {
+            clip.start();
+        }
+    }
+
+    public Clip getAmbienceClip() {
+        return this.ambienceClip;
     }
 
     private URL getURL(final String url) {
         return this.getClass().getClassLoader().getResource(url);
     }
 
-    private void play(final URL url, final float volume, final boolean loop) 
+    private void play(final AudioInputStream audioIN, final Clip clip, final boolean loop) 
     throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        final AudioInputStream audioIN = AudioSystem.getAudioInputStream(url);
-        final Clip clip = AudioSystem.getClip();
 
         clip.open(audioIN);
-        setVolume(clip, volume);
 
         audioIN.close();
         if (loop) {
@@ -82,10 +102,5 @@ public final class Sound {
         } else {
             clip.start();
         }
-    }
-
-    private void setVolume(final Clip clip, final float volume) {
-        final FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        volumeControl.setValue(volume);
     }
 }
