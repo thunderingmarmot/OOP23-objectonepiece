c9d84d4eb438cac93df550658b6bac5cf0a9b8ed
Matteo Giorgini <matteo.giorgini12@studio.unibo.it>
2024-02-18 21:49:43 +0100

    Fixed checkstyle error in test

--- src/test/java/it/unibo/object_onepiece/NavigationSystemTest.java
+++ src/test/java/it/unibo/object_onepiece/NavigationSystemTest.java
@@ -6,40 +6,35 @@ import it.unibo.object_onepiece.model.Utils.Position;
 import it.unibo.object_onepiece.model.Compass;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-
-public class NavigationSystemTest {
+class NavigationSystemTest {
     private final NavigationSystem nav = new Compass();
-    private static final Position POS1 = new Position(3,2);
+    private static final Position POS1 = new Position(3, 2);
     private static final Position POS2 = new Position(4, 2);
     private static final Position POS3 = new Position(2, 2);
     private static final Position POS4 = new Position(3, 3);
     private static final Position POS5 = new Position(3, 1);
-  
-    private static final Position DPOS1 = new Position(2,1);
-    private static final Position DPOS2 = new Position(4,3);
-    private static final Position DPOS3 = new Position(4,1);
-    private static final Position DPOS4 = new Position(2,3);
 
+    private static final Position DPOS1 = new Position(2, 1);
+    private static final Position DPOS2 = new Position(4, 3);
+    private static final Position DPOS3 = new Position(4, 1);
+    private static final Position DPOS4 = new Position(2, 3);
 
     @Test
-    void testDirection(){
+    void testDirection() {
         assertEquals(CardinalDirection.SOUTH, nav.move(POS2, POS1));
         assertEquals(CardinalDirection.NORTH, nav.move(POS3, POS1));
         assertEquals(CardinalDirection.EAST, nav.move(POS4, POS1));
         assertEquals(CardinalDirection.WEST, nav.move(POS5, POS1));
 
-        assertTrue(List.of(CardinalDirection.NORTH ,CardinalDirection.WEST).contains(nav.move(DPOS1, POS1)));
-        assertTrue(List.of(CardinalDirection.SOUTH ,CardinalDirection.EAST).contains(nav.move(DPOS2, POS1)));
-        assertTrue(List.of(CardinalDirection.SOUTH ,CardinalDirection.WEST).contains(nav.move(DPOS3, POS1)));
-        assertTrue(List.of(CardinalDirection.NORTH ,CardinalDirection.EAST).contains(nav.move(DPOS4, POS1)));
-        
+        assertTrue(List.of(CardinalDirection.NORTH, CardinalDirection.WEST).contains(nav.move(DPOS1, POS1)));
+        assertTrue(List.of(CardinalDirection.SOUTH, CardinalDirection.EAST).contains(nav.move(DPOS2, POS1)));
+        assertTrue(List.of(CardinalDirection.SOUTH, CardinalDirection.WEST).contains(nav.move(DPOS3, POS1)));
+        assertTrue(List.of(CardinalDirection.NORTH, CardinalDirection.EAST).contains(nav.move(DPOS4, POS1)));
     }
 }
